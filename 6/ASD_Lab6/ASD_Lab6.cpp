5c59968f0308574a67c664ce851f18cf0be4968d
Wandskape <name.silv@yandex.by>
2023-10-23 17:01:18 +0300

    increased functionality of Lab 6
    added encoding from file and writing to encoding file

--- 6/ASD_Lab6/ASD_Lab6.cpp
+++ 6/ASD_Lab6/ASD_Lab6.cpp
@@ -31,7 +31,7 @@ struct compare {
 	bool operator()(Node* l, Node* r) { return (l->freq > r->freq); }
 };
 
-void storeCodes(struct Node* root, string str)
+void storeCodes(struct Node* root, string str)//генерация кодов
 {
 	if (root == NULL)
 		return;
@@ -43,7 +43,7 @@ void storeCodes(struct Node* root, string str)
 
 priority_queue<Node*, vector<Node*>, compare> minHeap;
 
-void HuffmanCodes()
+void HuffmanCodes()//генерация дерева
 {
 	struct Node* left, * right, * top;
 	for (map<char, int>::iterator itr = freq.begin(); itr != freq.end(); itr++)
@@ -74,6 +74,23 @@ void calcFreq(string str)
 	}
 }
 
+void calcFreqFile()
+{
+	string bufferString;
+	ifstream input("input.txt");
+	while (getline(input, bufferString)) {
+		for (int i = 0; i < bufferString.size(); i++)
+			freq[bufferString[i]]++;
+	}
+	input.close();
+	
+	cout << "Частота вхождения символов:\n";
+	for (map<char, int>::iterator itr = freq.begin(); itr != freq.end(); ++itr)
+	{
+		cout << "Символ: " << itr->first << " - " << "Частота: " << itr->second << endl;
+	}
+}
+
 string decodeString(struct Node* root, string s)
 {
 	string ans = "";
@@ -92,25 +109,128 @@ string decodeString(struct Node* root, string s)
 	return ans + '\0';
 }
 
+void CodingFile() {
+	string bufferString;
+	string codingString;
+	ifstream input("input.txt");
+	ofstream output;
+	output.open("output.txt");
+	while (getline(input, bufferString)) {
+		for (auto i : bufferString)
+			codingString += codes[i];
+		output << codingString << "\n";
+		codingString = "";
+	}
+	input.close();
+	output.close();
+	cout << "\nФайл закодирован!\n" << endl;
+}
+
+void decodeFile(struct Node* root)
+{
+	string bufferString;
+	string encodingString;
+	ifstream input("output.txt");
+	ofstream output;
+	output.open("encoding.txt");
+	string ans = "";
+	struct Node* curr = root;
+
+	while (getline(input, bufferString)) {
+		for (int i = 0; i < bufferString.size(); i++) {
+			if (bufferString[i] == '0')
+				curr = curr->left;
+			if (bufferString[i] == '1')
+				curr = curr->right;
+			if (curr->left == NULL && curr->right == NULL) {
+				encodingString += curr->data;
+				curr = root;
+			}
+			if (bufferString[i] == '\n')
+				break;
+		}
+		output << encodingString << "\n";
+		encodingString = "";
+	}
+	input.close();
+	output.close();
+	cout << "\nФайл раскодирован!\n" << endl;
+}
+
+void BinaryCoding() {
+	calcFreqFile();
+	ifstream f("input.txt", ios::out | ios::binary);
+
+	f.clear(); f.seekg(0); // перемещаем указатель снова в начало файла
+
+	ofstream g("coding.txt", ios::out | ios::binary);
+
+	int count = 0; char buf = 0;
+	while (!f.eof())
+	{
+		char c = f.get();
+		vector<bool> x = table[c];
+		for (int n = 0; n < 2; n++)//x.size()
+		{
+			buf = buf | x[n] << (7 - count);
+			count++;
+			if (count == 8) { count = 0;   g << buf; buf = 0; }
+		}
+	}
+
+	f.close();
+	g.close();
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	int answer;
+	cin >> answer;
+	switch (answer)
+	{
+		case 1: {
+			string str = "Коломейчук Арсений Алексеевич";
+			string encodedString, decodedString;
+			//cout << "Введите строку которую хотите закодировать: ";
+			//getline(cin,str);
+			calcFreq(str);
+			HuffmanCodes();
+			cout << "\nТаблица символов и кодов:\n";
+			for (map<char, string>::iterator itr = codes.begin(); itr != codes.end(); itr++)
+				cout << "Символ: " << itr->first << " - " << "Код: " << itr->second << endl;
 
-	string str = "Коломейчук Арсений Алексеевич";
-	string encodedString, decodedString;
-	calcFreq(str);
-	HuffmanCodes();
-	cout << "\nТаблица символов и кодов:\n";
-	for (map<char, string>::iterator itr = codes.begin(); itr != codes.end(); itr++)
-		cout << "Символ: " << itr->first << " - " << "Код: " << itr->second << endl;
+			for (auto i : str)
+				encodedString += codes[i];
 
-	for (auto i : str)
-		encodedString += codes[i];
+			cout << "\nЗакодированная строка:\n" << encodedString << endl;
 
-	cout << "\nЗакодированная строка:\n" << encodedString << endl;
+			decodedString = decodeString(minHeap.top(), encodedString);
+			cout << "\nРаскодированная строка:\n" << decodedString << endl;
+			break;
+		}
+		case 2: {
+			calcFreqFile();
+			HuffmanCodes();
+			cout << "\nТаблица символов и кодов:\n";
+			for (map<char, string>::iterator itr = codes.begin(); itr != codes.end(); itr++)
+				cout << "Символ: " << itr->first << " - " << "Код: " << itr->second << endl;
+			CodingFile();
+			decodeFile(minHeap.top());
+			break;
+		}
+		case 3: {
+			calcFreqFile();
+			HuffmanCodes();
+			cout << "\nТаблица символов и кодов:\n";
+			for (map<char, string>::iterator itr = codes.begin(); itr != codes.end(); itr++)
+				cout << "Символ: " << itr->first << " - " << "Код: " << itr->second << endl;
+			BinaryCoding();
+			//decodeFile(minHeap.top());
+			break;
+		}
 
-	decodedString = decodeString(minHeap.top(), encodedString);
-	cout << "\nРаскодированная строка:\n" << decodedString << endl;
-	return 0;
+	default:
+		break;
+	}
 }
